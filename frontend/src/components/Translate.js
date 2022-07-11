79f83cb71d478eb2e69820fe99cfb00918929c70
sayihrudai <saihrudai11@gmail.com>
2022-07-11 16:25:41 +0530

    add components folders  ~hrudai

---
+++ frontend/src/components/Translate.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Translate = () => {
+  return (
+    <div>Section - 2</div>
+  )
+}
+
+export default Translate
---
+++ frontend/src/components/recentTranslation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const recentTranslation = () => {
+  return (
+    <div>
+      <h1>Section - 3</h1>
+    </div>
+  );
+};
+
+export default recentTranslation;
